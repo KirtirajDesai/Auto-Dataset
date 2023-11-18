da4fb7c692cb17129b7d283af416c042c376c391
Kirtiraj Desai <kirtirajdesai@gmail.com>
2023-11-18 17:50:58 +0530

    Adding Auto Datset for AWS Deployment

---
+++ new_project/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template,request
+from app.utils import Prediction 
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    return render_template("car_html.html")
+
+@app.route('/predict', methods = ["POST","GET"])
+def predict_price():
+    data = request.form
+    pred_obj = Prediction()
+    predicted_price = pred_obj.predict_price(data)
+    print(predicted_price)
+    
+    return str(predicted_price)
+
+@app.route("/kirtiraj")
+def myname():
+    return "testing github with multiuser"
+
+if __name__ == "__main__":
+    app.run(debug=False, port = 5000, host='0.0.0.0')
